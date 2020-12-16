89f1b5532f08790b25ab6f5255720aa0a8abe692
spanyashamasa <spanyashamasa@gmail.com>
2020-12-16 15:09:59 -0600

    asdhbasd

--- topdog/src/App.js
+++ topdog/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { HomePage } from './components/homepage';
+// import { 
+//   Navbar,
+//   Nav,
+//   Form,
+//   Button,
+//   FormControl,
+// } from 'react-bootstrap'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    
+    <Router>
+      <div className="navbar">
+        <a href="/about">
+          <img src="logo.svg" alt=" " height="25px" width="25px" />
         </a>
-      </header>
-    </div>
+        <a href="/">
+          Home
+        </a>
+
+      </div>
+
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <HomePage />
+            <About/>
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
-export default App;
+
+function About() {
+  return <h2>About</h2>;
+}
+
+
+//export default App;
