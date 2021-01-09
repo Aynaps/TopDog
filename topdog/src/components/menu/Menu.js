b7c5a5040eecec955957c78beda6158aa689247a
Mersim Rizmani <mersimrizmani@gmail.com>
2021-01-08 21:25:41 -0600

    added a pop-up side menu, and files for about page

---
+++ topdog/src/components/menu/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { bool } from 'prop-types';
+import { StyledMenu } from './Menu.styled';
+const Menu = ({ open }) => {
+  return (
+    <StyledMenu open={open}>
+        <a href="/">Home</a>
+        <a href="/userprofile">Profile</a>
+        <a href="/">Settings</a>
+        <a href="/">About TopDog</a>
+        <a href="/">Contact Us</a>
+        <a href="/">Logout</a>
+    </StyledMenu>
+  )
+}
+Menu.propTypes = {
+  open: bool.isRequired,
+}
+export default Menu;
--- topdog/src/components/userpage/userpage.js
+++ topdog/src/components/userpage/userpage.js
@@ -1,42 +1,51 @@
 import './userpage.css';
-
+import React, { useState , useRef } from 'react';
+import { useOnClickOutside } from '../../hooks';
+import Burger from '../burger/Burger';
+import Menu from '../menu/Menu';
 
 export default function UserPage() {
+    const [open, setOpen] = useState(false);
+    const node = useRef();
+    useOnClickOutside(node, () => setOpen(false));
     return (
-        <div className="userpagebody">
-
-            <div className="titleContainer">
+        <div>
+            <header className="userPageHeader">
+                <div className="titleContainer">
                     <a href= "/" className="Title">TopDog</a>
-            </div>
-
-            <div className="navbar">
-                <img src="dog.png" alt="Doggo"></img>
-                
-                <div className="searchbar-container">
-                    <input type="text" placeholder="Search.."></input>
-                    <img src="menu.png" alt="Menu"></img> 
                 </div>
-                
-                
-            </div>
 
-            <div className="userInfo">
-                <img src="stockphoto.jpg" height="150px" width="150px" margin="10px"></img>
-                    <p className="name">Name</p>
-                    <p className="username">@username</p>
-                <p>bio</p>
-                <p> <b>#num</b> posts  <b>#num</b> followers  <b>#num</b> following</p>
-            </div>
+                <div className="navbar">
+                    <img src="dog.png" alt="Doggo"></img>
+                        
+                    <div className="searchbar-container" ref={node}>
+                        <input type="text" placeholder="Search.."></input>
+                        <Burger open={open} setOpen={setOpen}/>
+                        <Menu open={open} setOpen={setOpen}/>
+                    </div>        
+                </div>
+            </header>
+            <div className="userpage">
 
-            <div className="userPosts">
-                <p>stuff</p>
-                <p>more stuff</p>
-                <p>this will fill automatically with the required size it needs</p>
-                <p>im showing it to you this way because we dont have title cards for dog photos yet</p>
-            </div>
+                <div className="userpagebody">
 
+                    <div className="userInfo">
+                        <img src="stockphoto.jpg" height="150px" width="150px" margin="10px"></img>
+                            <p className="name">Name</p>
+                            <p className="username">@username</p>
+                        <p>bio</p>
+                        <p> <b>#num</b> posts  <b>#num</b> followers  <b>#num</b> following</p>
+                    </div>
 
+                    <div className="userPosts">
+                        <p>stuff</p>
+                        <p>more stuff</p>
+                        <p>this will fill automatically with the required size it needs</p>
+                        <p>im showing it to you this way because we dont have title cards for dog photos yet</p>
+                    </div>
+                    
+                </div>
+            </div>           
         </div>
-
     );
 }
