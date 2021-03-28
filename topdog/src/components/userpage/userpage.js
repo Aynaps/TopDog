90ef98490ca4cacb492344c9ee792c3c6bd72aa8
vittorial12 <laudandovittoria@gmail.com>
2021-03-28 15:08:56 -0500

    updated userpage to get a users posts

--- topdog/src/components/userpage/userpage.js
+++ topdog/src/components/userpage/userpage.js
@@ -2,13 +2,40 @@ import './userpage.css';
 import Header from '../header/header';
 import React, { useState , useRef } from 'react';
 import { useOnClickOutside } from '../../hooks';
-import Burger from '../burger/Burger';
-import Menu from '../menu/Menu';
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+// Get all posts from specified user
+async function getPostsFromUsers(user) {
+    const getAllPosts = await firebase.firestore().collection('/posts').get();
+
+    // list all images from Posts
+    const images = [];
+
+    getAllPosts.forEach(element => {
+        if (element.data().postedBy === user) {
+            console.log(element.data().postedBy);  // user
+            var imgID = element.data().imageID;
+            console.log(imgID);  // users images 
+
+            images.push(imgID); // all users images
+        }
+    });
+    return images;
+    // return getAllPosts.docs.map(doc => doc.data());
+}
 
 export default function UserPage() {
     const [open, setOpen] = useState(false);
     const node = useRef();
     useOnClickOutside(node, () => setOpen(false));
+
+    getPostsFromUsers('sshama4')
+    .then((value) => console.log(value))    
+    .catch(e => {
+      console.log('ERROR: ' + e.message);
+    });
+
     return (
         <div className="maindiv">
             <Header/>
